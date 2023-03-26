f2347904079719a7ef6f55796bae0c7e75804031
Masterain98 <i@irain.in>
2023-03-26 10:24:43 +0000

    Deploying to gh-pages from @ UIGF-org/UIGF-org.github.io@7beadc3b70fa14280633595dd925e71d612cbf45 🚀

---
+++ assets/mermaid-mindmap.esm.min-789a1817.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-e600824e.js";import"./framework-a8f9fb10.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-268cad43.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
